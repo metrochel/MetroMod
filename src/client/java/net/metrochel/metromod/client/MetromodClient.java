ecdb3c60085e52fe455733f2db6bf2b34e86eb37
Metrochel <ivan.gulkin2016@yandex.ru>
2024-07-29 19:39:41 +0300

    Первый коммит

---
+++ src/client/java/net/metrochel/metromod/client/MetromodClient.java
@@ -0,0 +1,10 @@
+package net.metrochel.metromod.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MetromodClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/net/metrochel/metromod/Metromod.java
@@ -0,0 +1,10 @@
+package net.metrochel.metromod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Metromod implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
